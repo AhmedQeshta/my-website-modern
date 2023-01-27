c208e425333c57b13b21a99f3d324230673f5207
AhmedQeshta <ahmedqeshta1999@gmail.com>
2023-01-27 17:07:09 +0200

    fix bugs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,6 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { useState } from 'react';
-import { QueryClient, QueryClientProvider, Hydrate } from '@tanstack/react-query';
-import { config } from '@/utils';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(() => new QueryClient(config));
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
-      </Hydrate>
-    </QueryClientProvider>
-  );
+  return <Component {...pageProps} />;
 }
